5891232d60609abf18c208ea731b62d8e632978c
Ben Cox <ben@benjojo.co.uk>
2014-02-22 14:38:55 +0000

    Added checking to see if BlueZ is installed or not.

--- Navi.sh
+++ Navi.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+command -v hciconfig >/dev/null 2>&1 || { echo >&2 "I require hciconfig but it's not installed.  Aborting."; exit 1; }
+command -v hcitool >/dev/null 2>&1 || { echo >&2 "I require hcitool but it's not installed.  Aborting."; exit 1; }
 hciconfig hci0 up
 while(true) do
 	hcitool scan | grep -o '\([0-9a-fA-F]\{2\}:\)\{5\}[0-9a-fA-F]\{2\}' | \
