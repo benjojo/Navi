70d87fda04a274f80ea07ed159b621c910e7d6ce
Ben Cox <ben@benjojo.co.uk>
2014-02-22 14:51:56 +0000

    make sure the name is helpful.

--- Navi.sh
+++ Navi.sh
@@ -2,6 +2,7 @@
 command -v hciconfig >/dev/null 2>&1 || { echo >&2 apt-get install bluez bluez-utils; exit 1; }
 command -v hcitool >/dev/null 2>&1 || { echo >&2 "I require hcitool but it's not installed.  Aborting."; exit 1; }
 hciconfig hci0 up
+hciconfig hci0 name "TurnOffBlueTooth"
 while(true) do
 	hcitool scan | grep -o '\([0-9a-fA-F]\{2\}:\)\{5\}[0-9a-fA-F]\{2\}' | \
 	while read i
