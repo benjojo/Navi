d8d69ec438766d9fbf278fbc2a7a1e77674e3ceb
Ben Cox <ben@benjojo.co.uk>
2014-02-22 14:23:48 +0000

    Added automatic connection to ANYONE who is in the area

--- Navi.sh
+++ Navi.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-hcitool scan | grep -o '\([0-9a-fA-F]\{2\}:\)\{5\}[0-9a-fA-F]\{2\}'
+while(true) do
+	hcitool scan | grep -o '\([0-9a-fA-F]\{2\}:\)\{5\}[0-9a-fA-F]\{2\}' | \
+	while read i
+	do
+		echo "Attempting to connect to $i"
+		echo "0000" | timeout 10 bluez-simple-agent hci0 $i
+	done
+	echo "New Round"
+done
